008d95c8d5fac10b0589705d11db62a0985412fc
RaffysWeb <r4faelsimoes@gmail.com>
2018-07-05 21:29:00 +1000

    initial server setup

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
