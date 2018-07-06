e08c351c0381d48145dc783f8b803e0eed5f6368
RaffysWeb <r4faelsimoes@gmail.com>
2018-07-06 15:49:50 +1000

    configure passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://raffy:passw0rd@ds227821.mlab.com:27821/expressserver'
+  mongoURI: 'mongodb://raffy:passw0rd@ds227821.mlab.com:27821/expressserver',
+  secretOrKey: 'passw0rd'
 };
